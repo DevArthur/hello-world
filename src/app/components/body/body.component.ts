0dde1cc21967d575fddda1579a48693ef43fa5b6
arturoLA <dev.lopez.alcaraz@gmail.com>
2020-10-09 09:14:31 -0500

    initial commit

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-body',
-    templateUrl: './body.component.html',
-    styleUrls: ['./body.component.css']
+    templateUrl: './body.component.html'    
 })
-export class BodyComponent {}
+export class BodyComponent {
+    mostrar = true;
+    heroes: string[] = ['Spiderman', 'Ironman', 'Venom'];
+    frase: any = {
+        mensaje: "Un gran poder requiere una gran responsabilidad.",
+        autor: "Ben Parker"
+    };
+}
